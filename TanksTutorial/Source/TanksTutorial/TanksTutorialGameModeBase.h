e5efca1b3005bd63658a52eb229e8e2bbf91b53d
PlasmaSnake <michaelsy34@gmail.com>
2017-12-18 01:19:37 -0500

    UE project created

---
+++ TanksTutorial/Source/TanksTutorial/TanksTutorialGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TanksTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSTUTORIAL_API ATanksTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
